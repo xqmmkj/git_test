88390a518e133247d81dbb371adf41850c280242
caoqi <2743968592@qq.com;>
2019-06-18 23:46:50 +0800

    add github test

--- src/com/git/test/GitTest.java
+++ src/com/git/test/GitTest.java
@@ -1,11 +1,41 @@
 package com.git.test;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.RepositoryBranch;
+import org.eclipse.egit.github.core.RepositoryId;
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.client.GitHubResponse;
+import org.eclipse.egit.github.core.service.RepositoryService;
+
 public class GitTest{
-	public static void main(String[] args) {
-		System.out.println("HelloWorld!");
-		System.out.println("HelloWorld2!");
-	}
 	
+	private GitHubClient client;
+
+	private GitHubResponse response;
+
+	public static void main(String[] args) throws IOException {
+		GitHubClient client = new GitHubClient();
+		client.setCredentials("2743968592@qq.com", "buhtig@007");
+		RepositoryId repo = new RepositoryId("xqmmkj","git_test"); 
+		
+		
+		RepositoryService RepositoryService = new RepositoryService(client);
+		
+		List<RepositoryBranch> branchList= RepositoryService.getBranches(repo);
+		
+		List<Repository>  repositoryList = RepositoryService.getRepositories();
+		
+		for(RepositoryBranch branch: branchList) {
+			System.out.println(branch.getName());
+		}
+		
+		for(Repository r: repositoryList) {
+			System.out.println(r.getName());
+		}
+	}
 }
 
 
